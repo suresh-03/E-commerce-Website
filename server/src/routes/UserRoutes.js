c7ae8f43446f886bb834b1ab18c8323004cefd5a
Suresh-03 <sureshkumar732003@gmail.com>
2023-10-07 06:08:31 +0530

    validation

--- server/src/routes/UserRoutes.js
+++ server/src/routes/UserRoutes.js
@@ -2,15 +2,19 @@ const express = require("express");
 const router = express.Router();
 const { signup, signin, requireSignin } = require("../controllers/users");
 const { signinAdmin } = require("../controllers/admin");
+const {
+  validateSignupRequest,
+  isRequestValidated,
+  validateSigninRequest,
+} = require("../validators/validate");
 
-
-router.post("/signin", signin);
-router.post("/signup", signup);
-router.post("/profile",requireSignin,(req,res) => {
-    res.json({user:"profile"});
-})
+router.post("/signin", validateSigninRequest, isRequestValidated, signin);
+router.post("/signup", validateSignupRequest, isRequestValidated, signup);
+router.post("/profile", requireSignin, (req, res) => {
+  res.json({ user: "profile" });
+});
 // admin routes
-router.post("/admin/signin",signinAdmin);
+router.post("/admin/signin",validateSigninRequest,isRequestValidated,signinAdmin);
 // router.post("/admin/signup",signupAdmin);
 
 module.exports = router;
---
+++ server/src/validators/validate.js
@@ -0,0 +1,25 @@
+const { check, validationResult } = require("express-validator");
+
+exports.validateSignupRequest = [
+  check("firstname").notEmpty().withMessage("enter the firstname"),
+  check("lastname").notEmpty().withMessage("enter the lastname"),
+  check("email").notEmpty().withMessage("email should not be empty"),
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("password should be atleast 8 characters"),
+];
+
+exports.validateSigninRequest = [
+  check("email").notEmpty().withMessage("email should not be empty"),
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("password should be atleast 8 characters"),
+];
+
+exports.isRequestValidated = (req, res, next) => {
+  if (validationResult(req).array().length > 0) {
+    const error = validationResult(req).array()[0].msg;
+    return res.status(400).json({ error });
+  }
+  next();
+};
